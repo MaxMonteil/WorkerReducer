08dc5b99c5eeec825df63ef0b6b40ce26da1a0d8
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-10-28 21:26:15 +0200

    Rename main file to main

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_INCLUDED
+#define _MAIN_H_INCLUDED
+
+#include "proc_utils.h"
+#include "definitions.h"
+
+#define REDUCER_AMNT 3
+
+int file_len (char*);
+
+#endif // _MAIN_H_INCLUDED

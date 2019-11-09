64123c04c594b4aaaeebbd3c575840a71a91fa22
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-11-09 16:01:46 +0200

    Add define guards

--- structs.h
+++ structs.h
@@ -1,6 +1,11 @@
+#ifndef _STRUCTS_H_INCLUDED
+#define _STRUCTS_H_INCLUDED
+
 typedef struct CountResult {
     long status;
     long CMPS;
     long CCE;
     long ECE;
 } count_result;
+
+#endif // _STRUCTS_H_INCLUDED

e9374422429e2c6bedffd54d5d5205a033342464
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-11-10 10:39:08 +0200

    Add enum for target comparison and struct for reducers

--- structs.h
+++ structs.h
@@ -1,11 +1,17 @@
 #ifndef _STRUCTS_H_INCLUDED
 #define _STRUCTS_H_INCLUDED
 
+typedef enum target { CMPS, CCE, ECE } Target;
+
 typedef struct CountResult {
-    long status;
-    long CMPS;
-    long CCE;
-    long ECE;
+    int CMPS;
+    int CCE;
+    int ECE;
 } count_result;
+
+typedef struct ReducerArgs {
+    int count;
+    Target target;
+} reducer_args;
 
 #endif // _STRUCTS_H_INCLUDED

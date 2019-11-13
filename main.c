1568d91fb3ad36cb93ab0d6ec545d6d9818274d3
Maximilien Monteil <maximilienmonteil@gmail.com>
2019-11-13 21:54:07 +0200

    Add basic calc of execution time

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main (int argc, char **argv) {
     int i, proc_amnt = 4;
+    clock_t start, end;
 
     // cast command line arg as integer
     char *p;
@@ -14,9 +16,13 @@ int main (int argc, char **argv) {
             return 1;
     }
 
+    start = clock();
     printf("Hello, World!\n");
     for (i = 0; i < proc_amnt; i++) printf("%d ", i);
     printf("\n");
+    end = clock();
+
+    printf("Elapsed time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
